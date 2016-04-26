fdd159861c3806d9cf040040c48c41c8f490d092
Aniruddha Nadkarni <anadkarni30@gatech.edu>
2016-04-26 03:30:34 -0400

    initial commit

---
+++ Rotten Potatoes/Rotten Potatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rotten Potatoes
+//
+//  Created by Aniruddha Nadkarni on 4/26/16.
+//  Copyright © 2016 Aniruddha Nadkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
